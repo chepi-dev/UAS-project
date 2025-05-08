c5b32c85e092e4397c71afab03df4071a85adac2
zee <fauzisetiawan124@gmail.com>
2025-05-08 14:13:37 +0700

    index test3

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-<?php
-echo "test";
-echo "cepi goblog";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STMIK Bandung</title>
+</head>
+<body>
+    <style> 
+
+    h1 {
+        text-align: center;
+        color: blue;
+    }
+
+    </style>
+    <h1>UNIVERSITAS STMIK Bandung </h1>
+    <br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Numquam vitae, minus molestiae eius tempora hic odio quae tempore 
+        aut non placeat aperiam et dolor esse cupiditate dolorem eligendi 
+        velit earum?</p>
+    <br>
 
 
-?>
+</body>
+</html>
